bfcf3c897a758a7992441f169dd35d00e5c3cb96
Ashon Woodbury <a.woodbury01@gmail.com>
2020-08-14 11:45:42 -0400

    uploading to gh

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string BeanType { get; set; }
+    }
+}
